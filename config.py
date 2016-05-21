4d703916c90d0222b5b5519b3aa88535170d40c2
Thomas Sanjurjo <sanjurjo7@gmail.com>
2016-05-21 19:31:56 -0400

    Added user logins using OpenID. Need to modify for OAuth

--- config.py
+++ config.py
@@ -7,6 +7,17 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+OAUTH_CREDENTIALS = {
+    'facebook': {
+        'id': '131150650254246',
+        'secret': 'e44489e006c039a99996388e4cde5a46'
+    },
+    'twitter': {
+        'id': 't724wQasNgcKWkmS4mYIjVs5G',
+        'secret': 'hCRV8Q2xd7sQ3t6vMpziqOHOf3e4kovQN78tCmIPnMBsvMv8wK'
+    }
+}
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
